089f28d6710b50e62adbc8a02ad2ef7002e7f48f
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2023-02-06 17:52:23 -0300

    iamgem adicionada

--- site/blaze-gpt/src/ProductDisplay/ProductDisplay.js
+++ site/blaze-gpt/src/ProductDisplay/ProductDisplay.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './ProductDisplay.css';
+import productImage from './phone.jpeg';
 
 const ProductDisplay = (props) => {
-return (
-  <div className="product-display">
-  <h1 className="product-display__title">{props.name}</h1>
-  <p className="product-display__description">{props.description}</p>
-  <a href={props.link} target="_blank" className="product-display__link btn">Assine Já!</a>
+  return (
+    <div className="product-display">
+      <h1 className="product-display__title">{props.name}</h1>
+      <p className="product-display__description">{props.description}</p>
+      <img src={productImage} alt={props.name} className="product-display__image" />
+    <a href={props.link} target="_blank" className="product-display__link btn">Assine Já!</a>
   </div>
 );
 };
 
 export default ProductDisplay;
-
